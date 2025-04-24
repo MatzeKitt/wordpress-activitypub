f3a0c9f94ff5bf5498e31d8d08149b211c2aba59
Matthias Kittsteiner <mk@kittmedia.com>
2025-04-24 16:28:03 +0200

    Use save_post action to correctly add our logic

--- integration/class-multisite-language-switcher.php
+++ integration/class-multisite-language-switcher.php
@@ -19,21 +19,25 @@ class Multisite_Language_Switcher {
 	 * Initialize the class, registering WordPress hooks.
 	 */
 	public static function init() {
-		\add_action( 'save_post_' . Outbox::POST_TYPE, array( self::class, 'ignore_outbox_post' ), 9 );
-		\add_action( 'save_post_' . Outbox::POST_TYPE, array( self::class, 'unignore_outbox_post' ), 11 );
+		\add_action( 'save_post', array( self::class, 'ignore_outbox_post' ), 9, 2 );
+		\add_action( 'save_post', array( self::class, 'unignore_outbox_post' ), 11, 2 );
 	}
 
 	/**
 	 * Short-circuit saving Multisite Language Switcher data for the Outbox post type.
 	 */
-	public static function ignore_outbox_post() {
-		\add_action( 'msls_main_save', '__return_null' );
+	public static function ignore_outbox_post( $post_id, $post ) {
+		if ( Outbox::POST_TYPE === $post->post_type ) {
+			\add_action( 'msls_main_save', '__return_null' );
+		}
 	}
 
 	/**
 	 * Remove short-circuit for Multisite Language Switcher data.
 	 */
-	public static function unignore_outbox_post() {
-		\remove_action( 'msls_main_save', '__return_null' );
+	public static function unignore_outbox_post( $post_id, $post ) {
+		if ( Outbox::POST_TYPE === $post->post_type ) {
+			\remove_action( 'msls_main_save', '__return_null' );
+		}
 	}
 }
